fde7a5fe67229ce2771ea8ee7e827f48802f8ed7
SergioMonteRi <sergioribeiropalermo@gmail.com>
2025-02-25 11:48:00 -0300

    feat: create header component and start creating home screen

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,3 +1,39 @@
+import { Play } from 'phosphor-react'
+
+import {
+  Separator,
+  FormContainer,
+  HomeContainer,
+  CountdownContainer,
+} from './styles'
+
 export const Home = () => {
-  return <h1>Home</h1>
+  return (
+    <HomeContainer>
+      <form>
+        <FormContainer>
+          <label htmlFor="task">Vou trabalhar em</label>
+          <input id="task" />
+
+          <label htmlFor="minutesAmount">durante</label>
+          <input id="minutesAmount" type="number" />
+
+          <span>minutos</span>
+        </FormContainer>
+
+        <CountdownContainer>
+          <span>0</span>
+          <span>0</span>
+          <Separator>:</Separator>
+          <span>0</span>
+          <span>0</span>
+        </CountdownContainer>
+
+        <button>
+          <Play size={24} />
+          Comecar
+        </button>
+      </form>
+    </HomeContainer>
+  )
 }
