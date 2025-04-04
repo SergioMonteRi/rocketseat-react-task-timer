72f5341a620b347f47947a43e77fc13be7c51c58
SergioMonteRi <sergioribeiropalermo@gmail.com>
2025-04-04 17:32:46 -0300

    feat: create cycles reducer set cycle as finished

--- src/contexts/cycleContext.tsx
+++ src/contexts/cycleContext.tsx
@@ -48,29 +48,40 @@ export function CycleContextProvider({
       console.log(action)
       console.log(state)
 
-      if (action.type === 'ADD_NEW_CYCLE') {
-        return {
-          ...state,
-          cycles: [...state.cycles, action.payload.newCycle],
-          activeCycleId: action.payload.newCycle.id,
-        }
+      switch (action.type) {
+        case 'ADD_NEW_CYCLE':
+          return {
+            ...state,
+            cycles: [...state.cycles, action.payload.newCycle],
+            activeCycleId: action.payload.newCycle.id,
+          }
+        case 'INTERRUPT_CURRENT_CYCLE':
+          return {
+            ...state,
+            cycles: state.cycles.map((cycle) => {
+              if (cycle.id === state.activeCycleId) {
+                return { ...cycle, interruptedDate: new Date() }
+              } else {
+                return cycle
+              }
+            }),
+            activeCycleId: null,
+          }
+        case 'MARK_CURRENT_CYCLE_AS_FINISHED':
+          return {
+            ...state,
+            cycles: state.cycles.map((cycle) => {
+              if (cycle.id === activeCycleId) {
+                return { ...cycle, finishedDate: new Date() }
+              } else {
+                return cycle
+              }
+            }),
+            activeCycleId: null,
+          }
+        default:
+          return state
       }
-
-      if (action.type === 'INTERRUPT_CURRENT_CYCLE') {
-        return {
-          ...state,
-          cycles: state.cycles.map((cycle) => {
-            if (cycle.id === state.activeCycleId) {
-              return { ...cycle, interruptedDate: new Date() }
-            } else {
-              return cycle
-            }
-          }),
-          activeCycleId: null,
-        }
-      }
-
-      return state
     },
     { cycles: [], activeCycleId: null },
   )
@@ -106,8 +117,6 @@ export function CycleContextProvider({
       },
     })
 
-    // setCycles((prevCycles) => [...prevCycles, newCycle])
-
     setAmountSecondsElapsed(0)
   }
 
@@ -137,16 +146,6 @@ export function CycleContextProvider({
               activeCycleId,
             },
           })
-
-          // setCycles((state) =>
-          //   state.map((cycle) => {
-          //     if (cycle.id === activeCycleId) {
-          //       return { ...cycle, finishedDate: new Date() }
-          //     } else {
-          //       return cycle
-          //     }
-          //   }),
-          // )
 
           setAmountSecondsElapsed(totalSeconds)
           clearInterval(interval)
